bdbfb2ddc48be9ca7b6935ac38d42c4f80de296f
Kaue-Francisco <kauedfrancisco30@gmail.com>
2023-02-08 09:16:39 -0300

    Boas Normas
    Otimizei o código e fiz um tratamento de string para mostrar duas casas depois da virgula

--- main.py
+++ main.py
@@ -5,50 +5,46 @@ import requests
 from flask import Flask, render_template
 import pprint
 
-# Definindo as cotações
-cotacao_dol = 'USD-BRL'
-cotacao_eur = 'EUR-BRL'
-cotacao_ien = 'JPY-BRL'
-cotacao_gbp = 'GBP-BRL'
-cotacao_chf = 'CHF-BRL'
-cotacao_cad = 'CAD-BRL'
-
 
 # Cotação para o  Americano
-r = requests.get(f'https://economia.awesomeapi.com.br/json/last/{cotacao_dol}')
+r = requests.get(f'https://economia.awesomeapi.com.br/json/last/USD-BRL')
 cotacao_tot = r.json()
 dolar = cotacao_tot['USDBRL']['bid']
+dolar = dolar[:4]
 
 
 # Cotação para o Euro
-r = requests.get(f'https://economia.awesomeapi.com.br/json/last/{cotacao_eur}')
+r = requests.get(f'https://economia.awesomeapi.com.br/json/last/EUR-BRL')
 cotacao_tot = r.json()
 euro = cotacao_tot['EURBRL']['bid']
+euro = euro[:4]
 
 
 # Cotação para o Iene
-r = requests.get(f'https://economia.awesomeapi.com.br/json/last/{cotacao_ien}')
+r = requests.get(f'https://economia.awesomeapi.com.br/json/last/JPY-BRL')
 cotacao_tot = r.json()
 ien = cotacao_tot['JPYBRL']['bid']
+ien = ien[:4]
 
 
 # Cotação para a Libra
-r = requests.get(f'https://economia.awesomeapi.com.br/json/last/{cotacao_gbp}')
+r = requests.get(f'https://economia.awesomeapi.com.br/json/last/GBP-BRL')
 cotacao_tot = r.json()
 gbp = cotacao_tot['GBPBRL']['bid']
+gbp = gbp[:4]
 
 
 # Cotação para Franco Suiço
-r = requests.get(f'https://economia.awesomeapi.com.br/json/last/{cotacao_chf}')
+r = requests.get(f'https://economia.awesomeapi.com.br/json/last/CHF-BRL')
 cotacao_tot = r.json()
 chf = cotacao_tot['CHFBRL']['bid']
-
+chf = chf[:4]
 
 # Cotação para Dolar Canadense
-r = requests.get(f'https://economia.awesomeapi.com.br/json/last/{cotacao_cad}')
+r = requests.get(f'https://economia.awesomeapi.com.br/json/last/CAD-BRL')
 cotacao_tot = r.json()
 cad = cotacao_tot['CADBRL']['bid']
-
+cad = cad[:4]
 
 
 # Inicialização do Python Flask, para demonstrar a cotação
